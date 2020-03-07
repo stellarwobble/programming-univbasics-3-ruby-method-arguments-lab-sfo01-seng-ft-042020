4f611d804a1a7eb023ff31ad29b95389eb6b5edb
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-07 06:17:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,11 +5,11 @@
 
 
 def introduction(name)
-  p "Hi,my name is #{name}"
+  puts "Hi,my name is #{name}"
 end
 
 def introduction_with_language(name, language)
-  p "Hi my name is #{name} and I am learning to program in #{languge}."
+  puts "Hi my name is #{name} and I am learning to program in #{languge}."
 end
 
 def introduction_with_language_optional(name, language = 'Ruby')
